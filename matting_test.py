9badf628cb11fdac9189fb187abb14bee1d56809
protossw512 <helloxinyao.wang@gmail.com>
2017-11-09 17:19:21 -0800

    modified test scrip

--- matting_test.py
+++ matting_test.py
@@ -52,12 +52,10 @@ def main(_):
     b_input = tf.concat([b_RGB,b_trimap],3)
 
     with tf.name_scope('part1') as scope:
-        pred_mattes, en_parameters = base_net(b_input, trainable=False, training=True)
+        pred_mattes, en_parameters = base_net(b_input, trainable=False, training=False)
     with tf.name_scope('part2') as scope:
-        ref_pred_mattes = refine_net(pred_mattes, b_RGB, trainable=False, training=True)
+        ref_pred_mattes = refine_net(pred_mattes, b_RGB, trainable=False, training=False)
 
-    final_pred_mattes = tf.add(pred_mattes, ref_pred_mattes)
-    
     tf.add_to_collection("pred_mattes", pred_mattes)
 
     
@@ -85,7 +83,7 @@ def main(_):
             trimap_files = dataset_trimap
         for trimap_file in trimap_files:
             trimap_path = os.path.join(dataset_trimap, trimap_file)
-            RGB_path = os.path.join(dataset_RGB, trimap_file[:-3]+'jpg')
+            RGB_path = os.path.join(dataset_RGB, trimap_file[:-3]+'png')
             trimap_img = misc.imread(trimap_path, 'L')
             ori_shape = trimap_img.shape
             trimap_img = np.expand_dims(np.expand_dims(misc.imresize(trimap_img.astype(np.uint8), [image_height, image_width], interp='nearest').astype(np.float32),2),0)
@@ -94,7 +92,7 @@ def main(_):
             image = np.concatenate([trimap_img, rgb_img], axis=3)
             feed = {input_images:image}
             train_start = timeit.default_timer()
-            pred_alpha, summary_str, step = sess.run([pred_mattes,summary_op,global_step], feed_dict = feed)
+            pred_alpha, summary_str, step = sess.run([ref_pred_mattes,summary_op,global_step], feed_dict = feed)
             summary_writer.add_summary(summary_str,global_step=step)
             pred_alpha = np.squeeze(pred_alpha)
             pred_alpha = misc.imresize(pred_alpha, ori_shape)
